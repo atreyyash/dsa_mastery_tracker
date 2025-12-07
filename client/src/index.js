2f18d9fb0f01accab0e8ed728c75a7dca5e118d0
Yash Atrey <atreyyash@gmail.com>
2025-12-07 19:29:29 +0530

    build fixed

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'antd/dist/reset.css';
+import store from './store';
+import { AuthProvider } from './context/AuthContext';
+import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </AuthProvider>
   </React.StrictMode>
 );
